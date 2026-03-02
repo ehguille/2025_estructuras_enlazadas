4628f36876f5c92a5431fd6ac8cc76427a71b9b7
guillermogb <guillermogb@profe208b.informatica.cifpaviles.pa>
2026-03-02 09:57:54 +0100

    Se añade estructura de la clase.

---
+++ src/ListaSimpleEnlazada.java
@@ -0,0 +1,42 @@
+/**
+ * Enunciado derivado de la siguiente URL:
+ * https://www.it.uc3m.es/java/2011-12/units/pilas-colas/guides/2/guide_es_solution.html
+ * Se ha modificado en clase.
+ * @param <T>
+ */
+
+
+public class ListaSimpleEnlazada<T> {
+
+	public class Nodo<T> {
+		public T contenido;
+		public Nodo<T> siguiente;
+		
+		public Nodo(T contenido) {
+			this.contenido=contenido;
+			this.siguiente=null;
+		}
+	}
+	
+	Nodo<T> cabecera;
+	
+	public ListaSimpleEnlazada(){
+		cabecera=null;
+	}
+	
+	public void insertar(T contenido) {
+		Nodo<T> nuevoNodo=new Nodo<T>(contenido);
+		if(cabecera==null)
+			cabecera=nuevoNodo;
+			
+	}
+	
+	public T extraer() {
+		
+	}
+	
+	public T consultar(int i) {
+		
+	}
+	
+}
